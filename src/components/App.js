ebebd2a52112a8d0558f23e15cf8691dc7119e92
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-02-07 17:27:30 +0000

    create app component, style background

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import "../styles/App.css";
+import { Link } from "react-router-dom";
+import logo from "../images/dog.png";
 
 const App = () => {
-  return <div id="App">Animal Top Trumps</div>;
+  return (
+    <div id="App">
+      <div id="title">
+        <Link to="/">
+          <img id="logo" src={logo} alt="Logo" width="70px" height="70px" />{" "}
+        </Link>
+        <Link to="/">
+          <h1>Animal Top Trumps</h1>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default App;
